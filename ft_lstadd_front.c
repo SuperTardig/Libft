c562e7f33a2192e94d684b4fc1683cec45b78bbd
Benjamin Perron <benjaminperron150@gmail.com>
2022-05-05 10:49:25 -0400

    repaired strnstr

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bperron <bperron@student.42quebec.>        +#+  +:+       +#+        */
+/*   By: bperron <bperron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 09:48:50 by bperron           #+#    #+#             */
-/*   Updated: 2022/04/06 11:06:18 by bperron          ###   ########.fr       */
+/*   Updated: 2022/04/20 14:08:21 by bperron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bperron <bperron@student.42quebec.>        +#+  +:+       +#+        */
+/*   By: bperron <bperron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 10:44:08 by bperron           #+#    #+#             */
-/*   Updated: 2022/04/05 14:22:16 by bperron          ###   ########.fr       */
+/*   Updated: 2022/05/05 10:47:38 by bperron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,22 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int	i;
-	int	j;
-	int	start;
-	int	len2;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
-	if (haystack == needle)
-		return ((char *) needle);
-	if (needle[0] == 0)
+	if (needle[0] == '\0')
 		return ((char *) haystack);
-	while (haystack[i] && len-- > 0)
+	while (haystack[i] && i < len)
 	{
 		j = 0;
-		start = i;
-		len2 = len;
-		while (haystack[i++] == needle[j++] && len2-- > 0)
-			if (needle[j + 1] == 0)
-				return ((char *) &haystack[start]);
-		else if (needle[j + 1] != haystack[i + 1])
-			break ;
+		while (haystack[i + j] == needle[j] && (i + j) < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *) &haystack[i]);
+			j++;
+		}
+		i++;
 	}
 	return (NULL);
 }
